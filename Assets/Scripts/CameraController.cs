05b550f465ee79f68ed97980d506bd94565bac17
itsmyko <michaelchan013@hotmail.com>
2015-07-26 17:18:24 -0400

    Initial working game
    This is just a scene with a controllable ball and a pickup item that
    rotates

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	// LateUpdate is run after Update is run, if at all
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float rotateSpeed;
+	public Vector3 vector = new Vector3(3, 3, 1);
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}

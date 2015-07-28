689715f8b044cc90b59f449eb71ac487591d941d
itsmyko <michaelchan013@hotmail.com>
2015-07-28 03:20:44 -0400

    Finished Tutorial
    Pickup objects can be picked up and counted. Once all have been picked
    up, a text message indicating you have won appears.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	
 	public float speed;
-	
+	public Text countText;
+	public Text winText;
+
 	private Rigidbody rb;
+	private int count;
 	
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 	
 	void FixedUpdate () {
@@ -19,4 +26,23 @@ public class PlayerController : MonoBehaviour {
 		
 		rb.AddForce (movement * speed);
 	}
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText ();
+		}
+		//Destroy(other.gameObject);
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win!";
+		}
+	}
+
+
 }
